3607949ca06b24492a17bd2d6a73f4b2101607d2
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-16 06:14:18 +0500

    main01

--- bin/list_placeholder01.dart
+++ bin/list_placeholder01.dart
@@ -4,5 +4,11 @@ Create a list with the following values: 1, 2, 3, 4, 5
 Return the List
 
 */
+List func(){
+  List x=[1,2,3,4,5];
+  return x;
+}
 
-void main() {}
+void main() {
+ print(func());
+}
