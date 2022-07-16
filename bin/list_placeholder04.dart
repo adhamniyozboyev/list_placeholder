de2d61a41d72d31e716ef99a3234926e1453f02f
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-16 09:17:08 +0500

    main04

---
+++ bin/list_placeholder04.dart
@@ -0,0 +1,16 @@
+/*
+Create function func 
+Create a list ranging from 0 to n.
+Argument int n
+Return the List
+
+*/
+List func(int n){
+  int s=0;
+  List c=[];
+  while (s<n){
+    s++;
+    c.add(s);
+  }return c;
+}
+void main() {print(func(3));}
