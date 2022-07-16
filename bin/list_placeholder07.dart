78bd69fb61e622b3f7ad312f568c0f55245eec45
adhamniyozboyev <adhamniyozboyev77@gmail.com>
2022-07-16 11:49:28 +0500

    main

--- bin/list_placeholder07.dart
+++ bin/list_placeholder07.dart
@@ -8,9 +8,10 @@ Return the List
 List func(int N, int K,int M){
   int f=0;
   List d=[];
-  while(N<M){
+  while(N<K){
+    
     d.add(N);
-    N=N+5;
+    N=N+M;
   }return d;
 }
-void main() {print(func(10,20,25));}
+void main() {print(func(10,45,5));}
